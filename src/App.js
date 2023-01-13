1703c1189775f7355ee7a98a89cb0e75fd339461
S-ishita <singhalishita016@gmail.com>
2023-01-13 20:46:32 +0530

    pwd icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 
 import './App.css';
+import { useState } from 'react';
 import img from './cover.png';
+import { Icon } from 'react-icons-kit'
+import {eye} from 'react-icons-kit/feather/eye'
+import {eyeOff} from 'react-icons-kit/feather/eyeOff'
 
 function App() {
+
+    const [type,setType]=useState('password');
+    const [icon,setIcon]=useState(eyeOff);
+
+    const handleToggle=()=>{
+      if(type==='password'){
+        setIcon(eye);
+        setType('text');
+      }
+      else{
+        setIcon(eyeOff);
+        setType('password');
+      }
+    }
+
   return (
     <div className="cover">
    
@@ -15,7 +34,11 @@ function App() {
             <p>Enter your username or email address</p>
             <input type="text" name="email"  placeholder="alison@cloudcast.com"></input>
             <p>Enter your Password</p>
-            <input type="password" name="password" placeholder="**********" ></input>
+            <div className="wrapper">
+              <input type={type} name="password" placeholder="**********" ></input>
+            <span onClick={handleToggle}> <div style={{ color: "#dddfe3" }}><Icon icon={icon} size={20}></Icon></div></span>
+            </div>
+            
             <div>
             <button type="button" class="button1">Forgot Password</button>
             </div>
